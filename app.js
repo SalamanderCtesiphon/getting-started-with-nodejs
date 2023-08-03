bf285a4dec34ba566be1713262beac14b71014da
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-03 15:38:29 -0500

    changed up the code to see the effects

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('Hello World!');
+const pathname = '/a/b/c';
+const search = '?d=e';
+const hash = '#fgh';
+const myURL = new URL(`https://example.org${pathname}${search}${hash}`);
+console.log(myURL.href);
