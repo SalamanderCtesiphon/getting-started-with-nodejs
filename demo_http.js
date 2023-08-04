468ae862fab740e72d51123fb9a1e32d69ed8b33
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-04 15:44:41 -0500

    initiated another simple http server

--- demo_http.js
+++ demo_http.js
@@ -2,6 +2,9 @@ var http = require('http');
 
 //create server object
 http.createServer(function (req, res) {
+  //if the response from the http server is supposed to be displayed as html
+  //you should include an http header with the correct content type
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("Hello World !"); //write a response to the client
   res.end();//end response
 }).listen(8080);//the server listens on port 8080
